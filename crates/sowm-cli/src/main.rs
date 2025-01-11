6ec50fc599da85fc067baf9759070ebac3fecdae
Spencer <spencer3035@gmail.com>
2025-01-11 14:31:50 -0700

    Started on cli

--- crates/sowm-cli/src/main.rs
+++ crates/sowm-cli/src/main.rs
@@ -1,9 +1,40 @@
-use interprocess::local_socket::{prelude::*, GenericFilePath, ListenerOptions, Stream};
+use clap::{Parser, Subcommand};
+use interprocess::local_socket::{prelude::*, GenericFilePath, Stream};
 use std::io::{BufRead, BufReader, Write};
 
-use sowm_common::get_pipe_path;
+use sowm_common::{get_pipe_path, ClientMessage};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// Which command to send to the daemon
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Start cycling wallpapers
+    Start,
+    /// Stop cycling wallpapers
+    Stop,
+    /// Reset
+    Reset,
+}
+
+impl From<Command> for ClientMessage {
+    fn from(value: Command) -> Self {
+        match value {
+            Command::Start => ClientMessage::Start,
+            Command::Stop => ClientMessage::Stop,
+            Command::Reset => ClientMessage::Reset,
+        }
+    }
+}
 
 fn main() {
+    let cli = Cli::parse();
+    println!("{cli:#?}");
+
     let path = get_pipe_path();
     println!("Socket path is {}", path.display());
     let name = path.as_path().to_fs_name::<GenericFilePath>().unwrap();
--- crates/sowm-common/src/lib.rs
+++ crates/sowm-common/src/lib.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
 pub fn get_pipe_path() -> PathBuf {
@@ -11,32 +12,77 @@ pub fn get_pipe_path() -> PathBuf {
     p
 }
 
-#[derive(Debug)]
-pub enum Command {
-    Reset,
-    Stop,
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ClientMessage {
     Start,
-    Noop,
+    Stop,
+    Reset,
     // Add update config etc
 }
 
-pub fn parse_command(command: &str) -> Command {
-    for (ii, word) in command.split_whitespace().enumerate() {
-        if ii == 0 {
-            match word {
-                "reset" => return Command::Reset,
-                "stop" => return Command::Stop,
-                "start" => return Command::Start,
-                _ => return Command::Noop,
-            }
+impl ClientMessage {
+    pub fn serialize(&self) -> Vec<u8> {
+        bitcode::serialize(self).unwrap()
+    }
+    pub fn deserialize(v: &[u8]) -> Self {
+        bitcode::deserialize(v).unwrap()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ServerMessage {
+    Ok,
+    InvalidCommand,
+    DirNotFound,
+    NoImagesFound,
+}
+
+impl ServerMessage {
+    pub fn serialize(&self) -> Vec<u8> {
+        bitcode::serialize(self).unwrap()
+    }
+    pub fn deserialize(v: &[u8]) -> Self {
+        bitcode::deserialize(v).unwrap()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub switch_interval_sec: u64,
+    pub shuffle: bool,
+    pub image_dir: Option<String>,
+}
+
+impl Config {
+    pub fn is_valid(&self) -> bool {
+        if let Some(path) = self.image_dir.as_ref() {
+            let path = PathBuf::from(path);
+            println!("Path: '{}'", path.display());
+            return matches!(path.try_exists(), Ok(true));
+        }
+
+        true
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            switch_interval_sec: 60 * 30,
+            shuffle: true,
+            image_dir: None,
         }
     }
-    Command::Noop
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn default_config_valid() {
+        assert!(Config::default().is_valid(), "Default config wasn't valid")
+    }
 
     #[test]
     fn pipe_dir_exists() {
